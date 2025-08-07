707321f8d97f68fafb758cfa99a4ad73cdc07450
柯楠 <2690311349@qq.com>
2025-08-07 14:56:34 +0800

    It's able to change your goal now

--- src/App.js
+++ src/App.js
@@ -7,15 +7,50 @@ import { nanoid } from "nanoid";
 
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+
+
+
+
 
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {// if this task has the same ID as the edited task
+      if (id === task.id) {// Copy the task and update its name
+        return { ...task, name: newName };
+      }// Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {// if this task has the same ID as the edited task
       if (id === task.id) {// use object spread to make a new object whose `completed` prop has been inverted
@@ -34,19 +69,19 @@ function App(props) {
 
 
 
-
-
-
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-    />
-  ));
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} tasks remaining`;
@@ -56,9 +91,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
